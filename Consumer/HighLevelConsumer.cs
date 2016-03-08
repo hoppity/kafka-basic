2351e83606f2f19fe7269a90454fde880c409072
Ducas Francis <ducas.francis@williamhill.com.au>
2016-03-08 11:58:26 +1100

    use consumer console app to test simple consumer

---
+++ Consumer/HighLevelConsumer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using Metrics;
+using SimpleKafka;
+
+namespace Consumer
+{
+    class HighLevelConsumer
+    {
+        public int Start(HighLevelConsumerOptions opts)
+        {
+            var zookeeperString = opts.ZkConnect;
+            var consumerGroupId = opts.Group;
+            var testTopic = opts.Topic;
+
+            var timer = Metric.Timer("Received", Unit.Events);
+            Metric.Config.WithReporting(r => r.WithConsoleReport(TimeSpan.FromSeconds(5)));
+
+            var handler = new AutoResetEvent(false);
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                handler.Set();
+            };
+
+            using (var client = new KafkaClient(zookeeperString))
+            {
+                var consumerGroup = client.Consumer(consumerGroupId);
+                using (var instance = consumerGroup.Join())
+                {
+                    instance.Subscribe(testTopic)
+                        .Data(message =>
+                        {
+                            var time = DateTime.UtcNow.Ticks;
+                            var value = long.Parse(message.Value);
+                            var diff = (time - value) / 10000;
+                            timer.Record(diff, TimeUnit.Milliseconds);
+                        })
+                        .Start();
+                    handler.WaitOne();
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Consumer/SimpleConsumer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using Metrics;
+using SimpleKafka;
+
+namespace Consumer
+{
+    class SimpleConsumer
+    {
+        public int Start(SimpleConsumerOptions opts)
+        {
+            var zookeeperString = opts.ZkConnect;
+            var testTopic = opts.Topic;
+            var partition = opts.Partition;
+            var offset = opts.Offset;
+
+            var timer = Metric.Timer("Received", Unit.Events);
+            Metric.Config.WithReporting(r => r.WithConsoleReport(TimeSpan.FromSeconds(5)));
+
+            var handler = new AutoResetEvent(false);
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                handler.Set();
+            };
+
+            Thread.Sleep(5000);
+            using (var client = new KafkaClient(zookeeperString))
+            using (var consumer = client.SimpleConsumer())
+            using (var stream = consumer.Subscribe(testTopic, partition, offset))
+            {
+                stream
+                    .Data(message =>
+                    {
+                        var time = DateTime.UtcNow.Ticks;
+                        var value = long.Parse(message.Value);
+                        var diff = (time - value) / 10000;
+                        timer.Record(diff, TimeUnit.Milliseconds);
+                    })
+                    .Error(e => Console.Error.WriteLine(e.Message))
+                    .Start();
+                handler.WaitOne();
+            }
+
+            return 0;
+        }
+    }
+}
